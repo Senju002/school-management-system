aba56e3207f4cabab0a70009afe5e3fe94fd9c7b
Oktonius Zevanya <oktoniuszevanya@gmail.com>
2025-01-29 19:23:56 +0700

    Login Page and changing the password requirement to 3 digits

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -6,7 +6,7 @@ import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
 import { Head, Link, useForm } from "@inertiajs/react";
-import { Button } from "@material-tailwind/react";
+import MikrosilLogo from "@/../../public/images/logo_2.png"
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -17,7 +17,7 @@ export default function Login({ status, canResetPassword }) {
 
     useEffect(() => {
         return () => {
-            reset("password");
+            reset("password");  
         };
     }, []);
 
@@ -37,77 +37,140 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-
-            {status && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    {status}
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={handleOnChange}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={handleOnChange}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            value={data.remember}
-                            onChange={handleOnChange}
-                        />
-                        <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+        <div className="flex flex-col md:flex-row py-[30vh] px-5 justify-center gap-10 items-center">
+            <Head title="Login" />
+            <div className="w-full md:w-1/3 mb-5 md:mb-0">
+                <img
+                    src={MikrosilLogo}
+                    alt="#"
+                    className="w-full h-auto"
+                />
+            </div>
+            <div className="w-full md:w-1/3">
+                <h1 className="font-bold text-2xl w-full mb-4 text-center md:text-left">
+                    Website Manajemen Jadwal dan Honor Unit Tenaga Pengajar
+                    Universitas Mikroskil
+                </h1>
+                <p className="text-center md:text-left mb-4">
+                    Sistem Informasi Manajemen Jadwal dan Honorarium Berbasis
+                    Website
+                </p>
+                <form onSubmit={submit}>
+                    <div className="mt-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="name"
                         >
-                            Forgot your password?
-                        </Link>
-                    )}
+                            Username
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={handleOnChange}
+                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
+                    <div className="mt-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            autoComplete="current-password"
+                            onChange={handleOnChange}
+                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                        <InputError
+                            message={errors.password}
+                            className="mt-2"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <button
+                            disabled={processing}
+                            type="submit"
+                            className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>  
+        // <GuestLayout>
+        //     <Head title="Log in" />
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+        //     {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+
+        //     <form onSubmit={submit}>
+        //         <div>
+        //             <InputLabel htmlFor="email" value="Email" />
+
+        //             <TextInput
+        //                 id="email"
+        //                 type="email"
+        //                 name="email"
+        //                 value={data.email}
+        //                 className="mt-1 block w-full"
+        //                 autoComplete="username"
+        //                 isFocused={true}
+        //                 onChange={handleOnChange}
+        //             />
+
+        //             <InputError message={errors.email} className="mt-2" />
+        //         </div>
+
+        //         <div className="mt-4">
+        //             <InputLabel htmlFor="password" value="Password" />
+
+        //             <TextInput
+        //                 id="password"
+        //                 type="password"
+        //                 name="password"
+        //                 value={data.password}
+        //                 className="mt-1 block w-full"
+        //                 autoComplete="current-password"
+        //                 onChange={handleOnChange}
+        //             />
+
+        //             <InputError message={errors.password} className="mt-2" />
+        //         </div>
+
+        //         <div className="block mt-4">
+        //             <label className="flex items-center">
+        //                 <Checkbox name="remember" value={data.remember} onChange={handleOnChange} />
+        //                 <span className="ml-2 text-sm text-gray-600">Remember me</span>
+        //             </label>
+        //         </div>
+
+        //         <div className="flex items-center justify-end mt-4">
+        //             {canResetPassword && (
+        //                 <Link
+        //                     href={route('password.request')}
+        //                     className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        //                 >
+        //                     Forgot your password?
+        //                 </Link>
+        //             )}
+
+        //             <PrimaryButton className="ml-4" disabled={processing}>
+        //                 Log in
+        //             </PrimaryButton>
+        //         </div>
+        //     </form>
+        // </GuestLayout>
     );
 }

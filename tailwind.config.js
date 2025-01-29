7049c778a1ca057ffb5b8505e2b32ef29fb9bf42
Senju002 <rajkaran.mei34@gmail.com>
2025-01-29 17:54:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                mobile: { max: "639px" }, // => @media (max-width: 639px) { ... }
+                tablet: { max: "1023px" }, // => @media (max-width: 1023px) { ... }
+                laptop: { max: "1279px" }, // => @media (max-width: 1279px) { ... }
+                desktop: { min: "1280px" }, // => @media (min-width: 1280px) { ... }
+            },
+            colors: {
+                primary: "#264653", // Add your primary color
+                primaryHover: "#10DDFB",
+                secondary: "#B8E986", // Add your secondary color
+                textColor: "#E8E3E7",
+                bgColor: "#f5f5f5",
+            },
+        },
+    },
+
+    plugins: [require("@tailwindcss/forms")],
+});
